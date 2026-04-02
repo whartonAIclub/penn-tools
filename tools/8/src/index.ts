4e334bce73c9b31556bfecfaa2d288e4eb759440
Riaga1998 <ritika.aga1998@gmail.com>
2026-04-01 22:49:43 -0400

    Add initial Career Canvas MVP

--- tools/8/src/index.ts
+++ tools/8/src/index.ts
@@ -1,2 +1,7 @@
 export { Tool8 } from "./Tool8.js";
 export type { Tool8Input, Tool8Output } from "./types.js";
+export {
+  CAREER_CANVAS_SYSTEM_PROMPT,
+  buildMonolithicPromptForHttpApi,
+  buildUserMessageForCareerCanvas,
+} from "./careerPrompt.js";
