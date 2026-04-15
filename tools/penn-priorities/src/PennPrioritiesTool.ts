e7b241391bc2110557bb8bbc64cb7a4964f260c6
Ananya Chandra <ananyachandra14@gmail.com>
2026-04-15 19:27:48 -0400

    Fixed build issues.

--- tools/penn-priorities/src/PennPrioritiesTool.ts
+++ tools/penn-priorities/src/PennPrioritiesTool.ts
@@ -8,7 +8,7 @@ export class PennPrioritiesTool extends Tool<PennPrioritiesInput, PennPriorities
     id: "penn-priorities",
     title: "Penn Priorities",
     description: "Centralize and prioritize tasks across Career, Academic, and Social commitments for Wharton students.",
-    image: "/tools/penn-priorities/icon.png",
+    image: "Default",
     contributors: ["Travon Martin"],
     version: "0.1.0",
     inceptionDate: "2026-04-15",
